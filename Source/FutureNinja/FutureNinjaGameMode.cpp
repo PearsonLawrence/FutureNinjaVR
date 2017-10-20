fc4aa483e62a72cf65c06e39e93b7dd05d079695
Pearson Lawrence <s179130@students.aie.edu.au>
2017-10-20 16:35:19 -0700

    build fix

--- Source/FutureNinja/FutureNinjaGameMode.cpp
+++ Source/FutureNinja/FutureNinjaGameMode.cpp
@@ -9,8 +9,7 @@ AFutureNinjaGameMode::AFutureNinjaGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	DefaultPawnClass = APawn::StaticClass();
 
 	// use our custom HUD class
 	HUDClass = AFutureNinjaHUD::StaticClass();
--- Source/FutureNinja/FutureNinjaHUD.cpp
+++ Source/FutureNinja/FutureNinjaHUD.cpp
@@ -9,9 +9,6 @@
 
 AFutureNinjaHUD::AFutureNinjaHUD()
 {
-	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
-	CrosshairTex = CrosshairTexObj.Object;
 }
 
 
